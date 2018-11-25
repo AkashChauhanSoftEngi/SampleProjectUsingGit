64263803d684db632134fcf93bd6f92116cac254
Sample <Sample>
2018-11-26 00:53:55 +0530

    Second commit.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,6 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Sample project, successful uploaded!");
+        System.out.println("Sample project, successful uploaded");
     }
 }
