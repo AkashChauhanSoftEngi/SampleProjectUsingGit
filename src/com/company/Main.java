77525be37decdcbf17fd0d69d2c7df66107a869e
Akash Chauhan <akashchauhan.sejapan@gmail.com>
2018-11-29 09:11:44 +0530

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Impact of the changes in remote to local directory!");
+        System.out.println("Success!");
     }
 }
