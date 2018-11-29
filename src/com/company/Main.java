d35570ace76abb6cfaf672901fa44197b0273a2f
Sample <Sample>
2018-11-29 09:34:33 +0530

    Commit Sample

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Impact of the changes in remote to local directory!");
+        System.out.println("Impact of the changes in remote to local directory");
         System.out.println("Success!");
     }
 }
