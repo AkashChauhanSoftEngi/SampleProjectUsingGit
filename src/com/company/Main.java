bffef4a2967a4fbf064c66b1d790ab5fe7789828
Sample <Sample>
2018-11-29 09:39:21 +0530

    Testing

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,10 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+<<<<<<< Updated upstream
         System.out.println("Sample project, successful uploaded!");
+=======
+        System.out.println("Impact of the changes in remote to local directory");
+>>>>>>> Stashed changes
     }
 }
