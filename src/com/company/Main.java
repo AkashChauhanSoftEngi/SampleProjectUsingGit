b657aee03e19b24619c0bf04ce3d76432acc9ecf
Sample <Sample>
2018-11-29 09:44:50 +0530

    Testing

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,6 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Impact of the changes in remote to local directory");
-        System.out.println("Success!");
+        System.out.println("Sample project, successful uploaded");
     }
 }
