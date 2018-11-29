9ace9ac4365c4bfc9c995b44af06c263a3d37240
Akash Chauhan <akashchauhan.sejapan@gmail.com>
2018-11-29 09:11:09 +0530

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,6 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Sample project!");
+        System.out.println("Impact of the changes in remote to local directory!");
     }
 }
