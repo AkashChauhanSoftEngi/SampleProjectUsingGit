5d2c9fa612e16cf10caa6b9ffd5470ac18a5b1d1
Akash Chauhan <akash.chauhan@broctagon.com>
2018-12-10 11:15:55 +0530

    Direct commit without staging changes.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Sample project, successful uploaded!");
+        System.out.println("Added!");
     }
 }
