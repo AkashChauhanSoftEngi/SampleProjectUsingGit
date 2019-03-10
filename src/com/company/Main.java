efcc161513a9605d0a5c8a75a5db9bc54c9e0799
Akash Chauhan <akash75457@gmail.com>
2019-03-10 12:04:51 +0530

    Modified!

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,5 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Sample project, successful uploaded!");
-        System.out.println("Hello!");
     }
 }
